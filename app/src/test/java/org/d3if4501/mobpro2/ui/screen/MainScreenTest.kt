e2a41257126a268af8373869278fc31306554a6d
roisafajar <roisalfarizi9@gmail.com>
2025-01-17 07:12:58 +0700

    3.3.Membuat UI test kategori umur

--- app/src/test/java/org/d3if4501/mobpro2/ui/screen/MainScreenTest.kt
+++ app/src/test/java/org/d3if4501/mobpro2/ui/screen/MainScreenTest.kt
@@ -6,16 +6,15 @@ import org.junit.Test
 
 class MainScreenTest {
 
-    @Test
-    fun getKategori() {
+        @Test
+        fun getKategori() {
+            val anak = getKategori(umur = 7)
+            assertEquals(Kategori.ANAK, anak)
 
-                val anak = getKategori(umur = 7)
-                assertEquals(Kategori.ANAK, anak)
+            val remaja = getKategori(umur = 17)
+            assertEquals(Kategori.REMAJA, remaja)
 
-                val remaja = getKategori(umur = 17)
-                assertEquals(Kategori.REMAJA, remaja)
-
-                val invalid = getKategori(umur = -1)
-                assertEquals(Kategori.INVALID, invalid)
-            }
+            val invalid = getKategori(umur = -1)
+            assertEquals(Kategori.INVALID, invalid)
         }
+    }
